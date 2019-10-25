ef1c8db75cdb3577bdafe7a02b96c4ac0ee4e080
Juan Gonzalez <jacksmirk@gmail.com>
2019-10-25 21:46:59 +0200

    Plot 2 code and plot

---
+++ plot2.R
@@ -0,0 +1,25 @@
+# Download and unzip the data
+link <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+download.file(link, "data_for_peer_assessment.zip")
+unzip("data_for_peer_assessment.zip")
+
+if ("summarySCC_PM25.rds" %in% dir()) {
+  # Import the data
+  NEI <- readRDS("summarySCC_PM25.rds")
+  
+  # Subset Baltimore data
+  baltimorePM25 <- subset(NEI, fips == "24510")
+  
+  # Sum the emissions per year
+  baltimoreTotalEmissionsPerYear <- aggregate(baltimorePM25$Emissions, by=list(year=baltimorePM25$year), FUN=sum)
+  names(baltimoreTotalEmissionsPerYear)[2] <- 'emissions' 
+  
+  # Initialize device png 480x480
+  png("plot2.png", width = 480, height = 480)
+  
+  # Generate plot
+  with(baltimoreTotalEmissionsPerYear, plot(year, emissions, type = 'l'))
+  
+  # Close device
+  dev.off()
+}
