e3eed2094302b4df2fda9a1b82c806c5e80b8d36
Juan Gonzalez <jacksmirk@gmail.com>
2019-10-25 21:33:33 +0200

    Plot 1 code and plot

---
+++ plot1.R
@@ -0,0 +1,22 @@
+# Download and unzip the data
+link <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+download.file(link, "data_for_peer_assessment.zip")
+unzip("data_for_peer_assessment.zip")
+
+if ("summarySCC_PM25.rds" %in% dir()) {
+  # Import the data
+  NEI <- readRDS("summarySCC_PM25.rds")
+  
+  # Sum the emissions per year
+  totalEmissionsPerYear <- aggregate(NEI$Emissions, by=list(year=NEI$year), FUN=sum)
+  names(totalEmissionsPerYear)[2] <- 'emissions' 
+  
+  # Initialize device png 480x480
+  png("plot1.png", width = 480, height = 480)
+  
+  # Generate plot
+  with(totalEmissionsPerYear, plot(year, emissions, type = 'l'))
+  
+  # Close device
+  dev.off()
+}
